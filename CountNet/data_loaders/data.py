770ba8c3c89b083b62e33e5a499f40ee724f1c15
Jeremias Traub <jtraub@iup.uni-heidelberg.de>
2020-07-03 11:12:22 +0200

    Add CrowdCountingDataset base class

--- CountNet/data_loaders/data.py
+++ CountNet/data_loaders/data.py
@@ -1,4 +1,4 @@
-""""""
+"""This module implements Crowd-Counting datasets."""
 
 import os
 
@@ -9,14 +9,19 @@ import matplotlib.pyplot as plt
 from torch.utils.data.dataset import Dataset
 
 # -----------------------------------------------------------------------------
+# Base class
 
-class MallDataset(Dataset):
-    """The mall dataset"""
+class CrowdCountingDataset(Dataset):
+    """Base class for Crowd-counting datasets. Derived classes overwrite the
+    ``_extract_data`` method.
+    """
+    # TODO Add ``transformation`` arg to allow passing transformations that are
+    #      applied to the images in ``__getitem__()`` (e.g. rescaling)
     def __init__(self, path_to_folder: str, *, train: bool=True):
-        """Initializes the MallDataset
+        """Initializes the dataset.
         
         Args:
-            path_to_folder (str): Path to the folder containing the mall-data
+            path_to_folder (str): Path to the folder containing the data
             train (bool, optional): If true, the training data is loaded. Else,
                 the test data is loaded.
         """
@@ -31,7 +36,34 @@ class MallDataset(Dataset):
         # TODO Store the data as HDF5 once and adapt this function to read
         #      from the HDF5 file. This is much quicker and allows for a
         #      uniform datastructure.
+        images, count, pos = self._extract_data()
 
+        self.data = images
+        self.count = count
+        self.pos = pos
+
+    def __getitem__(self, idx):
+        """Returns tuple of image, total count, and positions at given index"""
+        # TODO Find a consistent output format here
+        return self.data[idx], (self.count[idx], self.pos[idx])
+
+    def __len__(self):
+        return len(self.data)
+
+    def _extract_data(self):
+        """Extracts the image, count, and position data"""
+        raise NotImplementedError()
+
+# -----------------------------------------------------------------------------
+# Derived classes
+
+class MallDataset(CrowdCountingDataset):
+    """The mall dataset.
+
+    https://personal.ie.cuhk.edu.hk/~ccloy/downloads_mall_dataset.html
+    """
+    def _extract_data(self):
+        """Extracts the image, count, and position data"""
         # Assemble the paths to the image data and the ground truth
         frames_path = os.path.join(self.path, "frames/seq_"+"{idx:06d}"+".jpg")
         gt_path = os.path.join(self.path, "mall_gt.mat")
@@ -39,7 +71,7 @@ class MallDataset(Dataset):
         np.random.seed(0)
         train_idxs, test_idxs = np.split(np.random.permutation(2000), [1800])
 
-        if train:
+        if self.train:
             idxs = train_idxs
         else:
             idxs = test_idxs
@@ -53,16 +85,71 @@ class MallDataset(Dataset):
         # Load the groundtruth
         gt = loadmat(gt_path)
 
-        self.data = frames
-        self.count = gt['count'][:10]
-        # The positions are stored in a nested structure of arrays and tuples,
-        # hence the somewhat ugly item access...
-        self.pos = [gt['frame'][:,i][0][0][0][0] for i in range(10)]
+        images = frames
+        count = gt['count'][:10]
+        pos = [gt['frame'][:,i][0][0][0][0] for i in range(10)]
 
-    def __getitem__(self, idx):
-        """Returns tuple of image, total count, and positions at given index"""
-        # TODO Find a consistent output format here
-        return self.data[idx], (self.count[idx], self.pos[idx])
+        return images, count, pos
 
-    def __len__(self):
-        return len(self.data)
+
+class ShanghaiTechDataset(CrowdCountingDataset):
+    """The ShanghaiTech (part A/B) dataset.
+
+    https://www.kaggle.com/tthien/shanghaitech
+    """
+    def _extract_data(self):
+        """Extracts the image, count, and position data"""
+        if self.train:
+            data_path = os.path.join(self.path, "train_data")
+        else:
+            data_path = os.path.join(self.path, "test_data")
+
+        # Assemble the paths to the image data and the ground truth
+        image_path = os.path.join(data_path, "images/IMG_"+"{idx}"+".jpg")
+        gt_path = os.path.join(data_path, "ground-truth/GT_IMG_{idx}.mat")
+
+        # Load the images and groundtruth
+        # TODO For now, only import the first 10 images.
+        images = []
+        gt = []
+        for i in range(10):
+            images.append(plt.imread(image_path.format(idx=i+1)))
+            gt.append(loadmat(gt_path.format(idx=i+1)))
+
+        count = [label['image_info'].item().item()[1].astype('uint8')
+                      for label in gt]
+        pos = [label['image_info'].item().item()[0] for label in gt]
+
+        return images, count, pos
+
+
+class UFC_CC_50Dataset(CrowdCountingDataset):
+    """The UFC_CC_50 dataset.
+
+    https://www.kaggle.com/tthien/ucfcc50/data
+    """
+    def _extract_data(self):
+        """Extracts the image, count, and position data"""
+        # Assemble the paths to the image data and the ground truth
+        image_path = os.path.join(self.path, "{idx}.jpg")
+        gt_path = os.path.join(self.path, "{idx}_ann.mat")
+
+        np.random.seed(0)
+        train_idxs, test_idxs = np.split(np.random.permutation(50), [40])
+
+        if self.train:
+            idxs = train_idxs
+        else:
+            idxs = test_idxs
+
+        # Load the images and groundtruth
+        images = []
+        gt = []
+        for i in idxs:
+            images.append(plt.imread(image_path.format(idx=i+1)))
+            gt.append(loadmat(gt_path.format(idx=i+1)))
+
+        count = [len(label['annPoints']) for label in gt]
+        pos = [label['annPoints'] for label in gt]
+
+        return images, count, pos
