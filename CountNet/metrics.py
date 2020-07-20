1fcf8f7caf381814389aba5eda0c1d5b29beb649
Jeremias Traub <jtraub@iup.uni-heidelberg.de>
2020-07-20 23:18:37 +0200

    Initial commit

---
+++ CountNet/metrics.py
@@ -0,0 +1,21 @@
+"""Validation metrics"""
+
+import torch
+
+# -----------------------------------------------------------------------------
+
+def RMSE(x, y):
+    """Root Mean Squared Error"""
+    pass
+
+def GAME(x, y):
+    """Grid Average Mean absolute Error"""
+    pass 
+
+def PSNR(x, y):
+    """Peak Signal to Noise Ratio"""
+    pass 
+
+def SSI(x, y):
+    """Structural Similarity Index"""
+    pass 
