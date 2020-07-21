1bee401ebdf45e2891454947b498f7b9a839f61b
Jeremias Traub <jtraub@iup.uni-heidelberg.de>
2020-07-22 00:12:31 +0200

    Adjust Dataset classes and configurations

--- CountNet/data/datasets.py
+++ CountNet/data/datasets.py
@@ -5,17 +5,18 @@ from typing import Callable
 
 from torch.utils.data.dataset import Dataset
 from PIL import Image
+import h5py
 import numpy as np
-import pandas as pd
+from tqdm import tqdm
 
 # -----------------------------------------------------------------------------
 
 class CrowdCountingDataset(Dataset):
-    def __init__(self, data_path: str,
-                       mode: str,
-                       main_transform: Callable=None,
-                       img_transform: Callable=None,
-                       gt_transform: Callable=None):
+    def __init__(self, *, data_path: str,
+                          mode: str,
+                          main_transform: Callable=None,
+                          img_transform: Callable=None,
+                          gt_transform: Callable=None):
         """
         Args:
             data_path (str): Path to the dataset
@@ -42,15 +43,16 @@ class CrowdCountingDataset(Dataset):
         assert img_transform is None or callable(img_transform)
 
         # Load the data
+        self.data_path = data_path
         self.mode = mode
-        self.images, self.gt = self._load_data(data_path, mode)
+        self.images, self.gt = self._load_data()
 
         self.main_transform = main_transform
         self.img_transform = img_transform
         self.gt_transform = gt_transform
 
-        print(f"Initialized '{os.path.split(data_path)[1]}' dataset in "
-              f"{mode}ing mode (size: {len(self)}.")
+        print(f"Initialized '{os.path.split(self.data_path)[1]}' dataset in "
+              f"{self.mode}ing mode (size: {len(self)})")
 
     def __getitem__(self, idx):
         """Provides access via index."""
@@ -70,39 +72,130 @@ class CrowdCountingDataset(Dataset):
     def __len__(self):
         return len(self.images)
 
-    def _load_data(self, data_path, mode):
+    def _load_data(self):
+        raise NotImplementedError()
+
+class MallDataset(CrowdCountingDataset):
+    """The mall dataset.
+
+    https://personal.ie.cuhk.edu.hk/~ccloy/downloads_mall_dataset.html
+    """
+    def _load_data(self):
         """Load image data and ground-truth density maps."""
-        # TODO If the stored data has a uniform structure, do it as it is.
-        #      If *not*, use this as base class and derive from it (overwriting
-        #      the `_load_data` method in the derived Dataset classes.
-        # TODO Change depending on the storage format of train and test data.
-        #      Currently, expects train data at ".../img/train/" and
-        #      ".../den/train/".
-        img_path = os.path.join(data_path , 'img', mode)
-        gt_path = os.path.join(data_path, 'den', mode)
+        img_path = os.path.join(self.data_path , "frames", "seq_{idx:06d}.jpg")
+        gt_path = os.path.join(self.data_path, "density_maps.h5")
         
+        np.random.seed(0)
+        train_idxs, test_idxs = np.split(np.random.permutation(2000), [1600])
+
+        if self.mode == 'train':
+            idxs = train_idxs
+        else:
+            idxs = test_idxs
+
         images, gt = [], []
 
-        # FIXME `os.listdir` does not guarantee for correct ordering! This has
-        #       to be handled explicitly! (or sorted, if filenames allow for
-        #       that)
-        for fname in [f for f in os.listdir(img_path)
-                      if os.path.isfile(os.path.join(img_path, f))]:
-
-            img = Image.open(os.path.join(img_path, fname))
+        for i in tqdm(idxs,
+                      desc=f"Loading '{os.path.split(self.data_path)[1]}' "
+                           f"{self.mode}ing data..."):
+            # Load image
+            img = Image.open(img_path.format(idx=i+1))
             if img.mode == 'L':
                 img = img.convert('RGB')
 
-            den = pd.read_csv(os.path.join(gt_path,
-                                           os.path.splitext(fname)[0]+'.csv'),
-                              sep=',', header=None).values
-            den = den.astype(np.float32, copy=False)
-            den = Image.fromarray(den)
+            # Load density map
+            with h5py.File(gt_path, 'r') as f:
+                dm = f[str(i+1)][()]
             
+            # Store both
             images.append(img)
-            gt.append(den)
+            gt.append(dm)
 
-        images = np.stack(images)
-        gt = np.stack(gt)
+        return images, gt
+
+class ShanghaiTechDataset(CrowdCountingDataset):
+    """The ShanghaiTech (part A/B) dataset.
+
+    https://www.kaggle.com/tthien/shanghaitech
+    """
+    def __init__(self, *, part: str, **kwargs):
+        """"""
+        super().__init__(**kwargs)
+        self.part = part
+
+    def _load_data(self):
+        """Load image data and ground-truth density maps."""
+        if self.mode == 'train':
+            data_path = os.path.join(self.data_path, "train_data")
+        else:
+            data_path = os.path.join(self.data_path, "test_data")
+
+        img_path = os.path.join(data_path , "images", "IMG_{idx}.jpg")
+        gt_path = os.path.join(data_path, "density_maps.h5")
+
+        images, gt = [], []
+
+        if self.part=='A' and self.mode=='test':
+            num_images = 182
+        elif self.part=='A' and self.mode=='train':
+            num_images = 300
+        elif self.part=='B' and self.mode=='test':
+            num_images = 316
+        elif self.part=='B' and self.mode=='train':
+            num_images = 400
+
+        for i in tqdm(range(num_images),
+                      desc=f"Loading '{os.path.split(self.data_path)[1]}' "
+                           f"{self.mode}ing data..."):
+            # Load image
+            img = Image.open(img_path.format(idx=i+1))
+            if img.mode == 'L':
+                img = img.convert('RGB')
+
+            # Load density map
+            with h5py.File(gt_path, 'r') as f:
+                dm = f[str(i+1)][()]
+            
+            # Store both
+            images.append(img)
+            gt.append(dm)
+
+        return images, gt
+
+class UCF_CC_50Dataset(CrowdCountingDataset):
+    """The UCF_CC_50 dataset.
+
+    https://www.kaggle.com/tthien/ucfcc50/data
+    """
+    def _load_data(self):
+        """Load image data and ground-truth density maps."""
+        img_path = os.path.join(self.data_path , "UCF_CC_50", "{idx}.jpg")
+        gt_path = os.path.join(self.data_path, "density_maps.h5")
+        
+        np.random.seed(0)
+        train_idxs, test_idxs = np.split(np.random.permutation(50), [30])
+
+        if self.mode == 'train':
+            idxs = train_idxs
+        else:
+            idxs = test_idxs
+
+        images, gt = [], []
+
+        for i in tqdm(idxs,
+                      desc=f"Loading '{os.path.split(self.data_path)[1]}' "
+                           f"{self.mode}ing data..."):
+            # Load image
+            img = Image.open(img_path.format(idx=i+1))
+            if img.mode == 'L':
+                img = img.convert('RGB')
+
+            # Load density map
+            with h5py.File(gt_path, 'r') as f:
+                dm = f[str(i+1)][()]
+            
+            # Store both
+            images.append(img)
+            gt.append(dm)
 
         return images, gt
