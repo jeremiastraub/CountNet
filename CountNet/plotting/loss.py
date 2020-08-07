89bf69dc891b70212fb84c704981de50f6b4e2ad
Jeremias Traub <jtraub@iup.uni-heidelberg.de>
2020-08-07 21:30:08 +0200

    Use seaborn for loss plotting

--- CountNet/plotting/loss.py
+++ CountNet/plotting/loss.py
@@ -2,6 +2,7 @@
 
 from typing import List
 import matplotlib.pyplot as plt
+import seaborn as sns
 from .plot_utils import extract_loss_information
 
 # -----------------------------------------------------------------------------
@@ -19,14 +20,19 @@ def plot_loss_single(tag: str, datapath: str, outpath: str=None):
     """
     loss, steps, epoch = extract_loss_information(tag, datapath)
 
+    sns.set()
+    sns.set(font_scale=1.2)
+    # sns.set_style('ticks')
+
     fig, ax = plt.subplots()
     ax.plot(steps, loss)
+    ax.set_xlabel("epoch")
+    ax.set_ylabel("MSE Loss")
+
+    sns.despine()
 
     if outpath is not None:
         fig.savefig(outpath, bbox_inches='tight')
-
-    else:
-        plt.show()
 
     return fig, ax
 
@@ -55,13 +61,12 @@ def plot_loss_multiple(tags: List[str],
         loss, steps, epoch = extract_loss_information(tag, datapath)
         ax.plot(steps, loss, label=(labels[i] if labels_exist else None))
 
+    ax.set_xlabel("epoch")
+    ax.set_ylabel("MSE Loss")
     if labels_exist:
         ax.legend()
 
     if outpath is not None:
         fig.savefig(outpath, bbox_inches='tight')
-
-    else:
-        plt.show()
 
     return fig, ax
